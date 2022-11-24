3c057af095e83e13ebea1eb95cf77e5ae5f95689
Jarkko Ahvenniemi <jarkko.ahvenniemi@tuni.fi>
2022-11-24 17:30:49 +0200

    Add example test

---
+++ test/add.test.js
@@ -0,0 +1,24 @@
+import assert from 'assert';
+import add from "../library/src/add.js";
+
+describe('Number additions', function () {
+    describe('#add()', function () {
+
+      it('should add two integers together', function () {
+        assert.equal(add(1,2), 3);
+      });
+
+      it('should add integers and doubles together', function () {
+        assert.equal(add(1,2.4), 3.4);
+      });
+
+      it('should not accept NaN values', function () {
+        assert.equal(add(NaN,2), NaN);
+      });
+
+      it('Example fail', function () {
+        assert.equal(add(1,1), 0);
+      });
+
+    });
+  });
