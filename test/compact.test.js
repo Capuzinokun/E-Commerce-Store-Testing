e4b0f6221445ea96fa4890679577c42abfc8a473
Jarkko Ahvenniemi <jarkko.ahvenniemi@tuni.fi>
2022-12-17 19:42:12 +0200

    Change assert.equal() to assert.deepEqual()

--- test/compact.test.js
+++ test/compact.test.js
@@ -5,27 +5,27 @@ describe('Falsey value removal', function () {
     describe('#compact()', function () {
 
       it("Should return an array without 'false' values", function () {
-        assert.equal(compact([1,false,2,3]), [1,2,3]);
+        assert.deepEqual(compact([1,false,2,3]), [1,2,3]);
       });
 
       it("Should return an array without 'null' values", function () {
-        assert.equal(compact([1,null,2,3]), [1,2,3]);
+        assert.deepEqual(compact([1,null,2,3]), [1,2,3]);
       });
 
       it("Should return an array without 'zero' values", function () {
-        assert.equal(compact([1, 0, 0.0, -0, 0x0, 2, 3]), [1,2,3]);
+        assert.deepEqual(compact([1, 0, 0.0, -0, 0x0, 2, 3]), [1,2,3]);
       });
 
       it("Should return an array without 'undefined' values", function () {
-        assert.equal(compact([1, 2, undefined, 3]), [1,2,3]);
+        assert.deepEqual(compact([1, 2, undefined, 3]), [1,2,3]);
       });
 
       it("Should return an array without 'NaN' values", function () {
-        assert.equal(compact([1, 2, 3, NaN]), [1,2,3]);
+        assert.deepEqual(compact([1, 2, 3, NaN]), [1,2,3]);
       });
 
       it("Should work with multiple falsey values", function () {
-        assert.equal(compact([1, false, 2, null, 3, 0, 4, 0.0, 5, -0, 6, 0x0, 7, undefined, 8, NaN]), [1,2,3,4,5,6,7,8]);
+        assert.deepEqual(compact([1, false, 2, null, 3, 0, 4, 0.0, 5, -0, 6, 0x0, 7, undefined, 8, NaN]), [1,2,3,4,5,6,7,8]);
       });
 
     });
