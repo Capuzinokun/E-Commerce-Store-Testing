b7fc9ce9b61e5dfe4c30d3b1371790cd7913c58c
Jarkko Ahvenniemi <jarkko.ahvenniemi@tuni.fi>
2022-12-17 19:00:49 +0200

    Add more tests

--- test/add.test.js
+++ test/add.test.js
@@ -12,8 +12,16 @@ describe('Number additions', function () {
         assert.equal(add(1,2.4), 3.4);
       });
 
+      it('should not accept character values', function () {
+        assert.equal(add('s',2), NaN);
+      });
+
+      it('should not accept string values', function () {
+        assert.equal(add("asdasdasd",2), NaN);
+      });
+
       it('should not accept NaN values', function () {
-        assert.equal(add(NaN,2), NaN);
+        assert.equal(add(NaN, 2), NaN);
       });
 
     });
