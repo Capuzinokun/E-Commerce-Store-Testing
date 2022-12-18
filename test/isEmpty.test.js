1a9807dc267fb3b1a53ba41fdc96892b1f608600
Jarkko Ahvenniemi <jarkko.ahvenniemi@tuni.fi>
2022-12-18 18:58:18 +0200

    Add tests for isEmpty.js

---
+++ test/isEmpty.test.js
@@ -0,0 +1,40 @@
+import assert from 'assert';
+import isEmpty from "../library/src/isEmpty.js";
+
+describe('Array filtering', function () {
+    describe('#isEmpty()', function () {
+
+      it("Should detect an empty object", function () {
+        assert.equal(isEmpty(new Object()), true);
+      });
+
+      it("Should detect an empty map", function () {
+        assert.equal(isEmpty(new Map()), true);
+      });
+
+      it("Should detect an empty set", function () {
+        assert.equal(isEmpty(new Set()), true);
+      });
+
+      it("Should detect an empty string", function () {
+        assert.equal(isEmpty(""), true);
+      });
+
+      it("Should detect a non-empty object", function () {
+        assert.equal(isEmpty({test:"test"}), false);
+      });
+
+      it("Should detect a non-empty map", function () {
+        assert.equal(isEmpty(new Map([ ["key", "value"] ])), false);
+      });
+
+      it("Should detect a non-empty set", function () {
+        assert.equal(isEmpty(new Set(["test"])), false);
+      });
+
+      it("Should detect a non-empty string", function () {
+        assert.equal(isEmpty("test"), false);
+      });
+
+    });
+  });
